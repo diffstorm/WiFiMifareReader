8de6ead4278c48e8231ffd7787d8b998ea412b84
Cihan <cihanasik1389@gmail.com>
2020-04-10 13:13:38 +0300

    Reader Library
    Reader library has been edited.

--- SW/Reader/v1/include/Reader.h
+++ SW/Reader/v1/include/Reader.h
@@ -2,6 +2,9 @@
 #define __READER_H__
 
 #include "CardConfig.h"
+#include <MFRC522.h>
+
+#define KEY_SIZE 6
 
 typedef enum
 {
@@ -11,18 +14,41 @@ typedef enum
     AUTH_ERROR,
     CAN_NOT_READ_UID,
     UID_LEN_ERROR,
-    READ_ERROR
+    READ_ERROR,
+    WRITE_ERROR
 } READER_status_t;
 
+void Reader_Prepare_Key(char systemPwd[]);
+
+bool Reader_Mifare_SetKeys(byte command, MFRC522::MIFARE_Key *oldKeyA, MFRC522::MIFARE_Key *oldKeyB);
+
 void Reader_Init();
+
 void Reader_Sleep();
+
 void Reader_WakeUp();
+
 bool Reader_IsMifare();
-READER_status_t Reader_ReadBlock(int blockNumber,byte block[CARD_ROW_SIZE]);
+
+bool Reader_HasCard();
+
+bool Reader_IsRead_Card();
+
+
+
+bool Reader_Card_Authenticate(byte command, int trailerBlock, MFRC522::MIFARE_Key *key);
+
+bool Reader_Is_Trailer_Block(int blockNumber);
+
+READER_status_t Reader_ReadBlock(int blockNumber,byte block[CARD_ROW_SIZE],u8 len);
+
 READER_status_t Reader_WriteBlock(int blockNumber, byte arrayAddress[CARD_ROW_SIZE]);
-void Reader_Fill_List();
-bool Reader_ComparePassword();     //kart bilgileri gösterilecek
+
+u8 Reader_Get_Trailer_Block(int blockNumber);
+
+
 void READER_stop();
+
 bool READER_handler();
 
 #endif // __READER_H__
