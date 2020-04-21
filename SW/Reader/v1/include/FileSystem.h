565b858f9204f157a07326996507af7e08660b34
oguzhanalkis <61747851+oguzhanalkis@users.noreply.github.com>
2020-04-21 17:26:49 +0300

    fileSystem güncellendi
    Konuşulan değişiklikler yapıldı. Yorumlar eklendi. update güncellendi. Sistemde kalan boş alanı bulan fonk. yazıldı.

--- SW/Reader/v1/include/FileSystem.h
+++ SW/Reader/v1/include/FileSystem.h
@@ -1,35 +1,65 @@
 #ifndef __FILESYSTEM_H__
 #define __FILESYSTEM_H__
 #include <FS.h>
+#include <WString.h>
 
-bool FILE_create(char* name);
 
 bool FILE_Init();
 
-bool FILE_write_internal(File *f, void *context, u32 length);
-
-bool FILE_read_internal(File *f, void *context, u32 length);
-
-bool FILE_Write(char *name, void *context, u32 length);
-
-bool FILE_Read(char *name,void *context,u32 lenght, u32 pos);
-
+//! Append data to a file (this will add data to end of the file)
+/*!
+    \param name [output] : file name which the data will be added in
+    \param context [input] : the data which will be added
+    \param length [input] : how many bytes data will be written
+    \return : boolean, true if successful
+*/
 bool FILE_Append(char *name, void *context, u32 length);
 
-bool FILE_IsExists(char *name);
+//! Write context to a file (this will delete all data in the given file and add given length of data)
+/*!
+    \param name [output] : file name which the data will be written in
+    \param context [input] : the data which will be written
+    \param length [input] : how many bytes data will be written
+    \return : boolean, true if successful
+*/
+bool FILE_Write(char *name, void *context, u32 length);
 
-size_t FILE_check_size( char *name);
+
+//! Read data from the file from given position 
+/*!
+    \param name [input] : file name which the data will be read
+    \param context [output] : Read in to context
+    \param length [input] : how many bytes data will be read
+    \param pos [input] : beginning position of the read process in file  
+    \return : boolean, true if successful
+*/
+bool FILE_Read(char *name,void *context,u32 lenght, u32 pos);
+
+//! Updates a file from given position 
+/*!
+    \param name [output] : file name which the data will be written
+    \param context [input] : the data which will be written
+    \param length [input] : how many bytes data will be written
+    \param pos [input] : beginning position of the write process in file  
+    \return : boolean, true if successful
+*/
+bool FILE_update(char *name,void* context,u32 length,u32 pos);
+
+//! Gives all filenames in "/" directory 
+/*!
+    \return : string, filenames seperated with comma in a string
+*/
+String FILE_get_file_list();
+
+
+size_t FILE_get_dir_size(char* dir);
+
+size_t FILE_system_available_size();
 
 bool FILE_delete( char *name);
 
-void FILE_get_List();
+bool FILE_IsExists(char *name);
 
-bool FILE_rename(char* name,char* name1);
-
-bool FILE_update(char *name,void* context,u32 length,u32 pos);
-
-size_t FILE_dir_size(char* dir);
-
-
+size_t FILE_get_size( char *name);
 
 #endif
--- SW/Reader/v1/src/FileSystem.cpp
+++ SW/Reader/v1/src/FileSystem.cpp
@@ -44,14 +44,20 @@ bool FILE_Init()
 
     if(i > errorTop)
     {
-        SPIFFS.format();      //ESP reset attığında düzelme ihtimaline karşı, format kullanılmasa olur mu? 
-        ESP.restart();        //Çünkü dosyalar kaybedilecek. Ya da güncel dosyalar bir yerde saklanacak ve cihaza geri göderilecek mi?
+        SPIFFS.format();      
+        ESP.restart();        
     }
 
     return ret;    
 }
 
-
+//! Write data to a file (an internal function for fail safe structure)
+/*!
+    \param f [output] : file pointer -> written in to this file
+    \param context [input] : the data which will be written
+    \param length [input] : how many bytes data will be written
+    \return : boolean, true if successful
+*/
 bool FILE_write_internal(File *f, void *context, u32 length)
 {
     u32 offset = 0;
@@ -75,6 +81,13 @@ bool FILE_write_internal(File *f, void *context, u32 length)
     return (offset == length);
 }
 
+//! Read context from a file (an internal function for fail safe structure)
+/*!
+    \param f [input] : file pointer -> which data will be read from
+    \param context [output] : read data in to context 
+    \param length [input] : how many bytes data will be read
+    \return : boolean, true if successfula
+*/
 bool FILE_read_internal(File *f, void *context, u32 length)
 {
     u32 offset = 0;
@@ -83,7 +96,7 @@ bool FILE_read_internal(File *f, void *context, u32 length)
     
     while(offset < length && error > 0)
     {
-        read = f->read((u8 *)context + offset , length - offset);  //
+        read = f->read((u8 *)context + offset , length - offset);  
 
         if(read > 0)
         {
@@ -98,22 +111,14 @@ bool FILE_read_internal(File *f, void *context, u32 length)
     return (offset == length);    
 }
 
-bool FILE_create(char* name)
-{
-    bool ret=false;
 
-    File f = SPIFFS.open(name,"r+");
-
-    if(NULL != f)
-    {
-        ret=true;
-        f.close();
-    }
-
-    return f;
-}
-
-
+//! Append data to a file (this will add data to end of the file)
+/*!
+    \param name [input] : file name which the data will be added in
+    \param context [input] : the data which will be added
+    \param length [input] : how many bytes data will be written
+    \return : boolean, true if successful
+*/
 bool FILE_Append(char *name, void *context, u32 length)
 {
 
@@ -123,7 +128,7 @@ bool FILE_Append(char *name, void *context, u32 length)
 
     if(NULL != f)
     {
-        if(true==FILE_write_internal(&f, context, length))    
+        if(false != FILE_write_internal(&f, context, length))    
         {
             ret = true;
         }
@@ -134,6 +139,13 @@ bool FILE_Append(char *name, void *context, u32 length)
     return ret;
 }
 
+//! Write context to a file (this will delete all data in the given file and add given length of data)
+/*!
+    \param name [output] : file name which the data will be written in
+    \param context [input] : the data which will be written
+    \param length [input] : how many bytes data will be written
+    \return : boolean, true if successful
+*/
 bool FILE_Write(char *name, void *context, u32 length)
 {
 
@@ -143,7 +155,7 @@ bool FILE_Write(char *name, void *context, u32 length)
 
     if(NULL != f)
     {
-        if(true==FILE_write_internal(&f, context, length))
+        if(false != FILE_write_internal(&f, context, length))
         {
             ret = true; 
         }
@@ -154,11 +166,15 @@ bool FILE_Write(char *name, void *context, u32 length)
     return ret;
 }
 
-bool FILE_rename(char *name, char *name1)
-{
-    return SPIFFS.rename(name, name1);
-}
 
+//! Read data from the file from given position 
+/*!
+    \param name [input] : file name which the data will be read from
+    \param context [output] : Read in to context
+    \param length [input] : how many bytes data will be read
+    \param pos [input] : beginning position of the read process in file  
+    \return : boolean, true if successful
+*/
 bool FILE_Read(char *name, void *context, u32 length, u32 pos)
 {
 
@@ -168,51 +184,47 @@ bool FILE_Read(char *name, void *context, u32 length, u32 pos)
 
     if(NULL != f)
     {
-        Serial.println("dosya acildi");
-        if(true==f.seek(pos, SeekMode::SeekSet))
+        
+        if(false != f.seek(pos, SeekMode::SeekSet))
         {
-            Serial.println("seek valisti");
-            if(true==FILE_read_internal(&f, context, length))
+            
+            if(false != FILE_read_internal(&f, context, length))
             {
-                //f.read((u8 *)context,length);
-                Serial.println("readint calisti");
                 ret=true;
             }
         } 
 
-        f.close();
-        Serial.printf("ret: %d",ret);     
+        f.close();    
     }
 
     return ret;
 
 }
 
-
+//! Updates a file from given position
+/*!
+    \param name [output] : file name which the data will be written
+    \param context [input] : the data which will be written
+    \param length [input] : how many bytes data will be written
+    \param pos [input] : beginning position of the write process in file  
+    \return : boolean, true if successful
+*/
 bool FILE_update(char *name, void *context, u32 length, u32 pos)
 {
     bool ret = false;
 
     File f = SPIFFS.open(name, "r+");
-
+    
     if(NULL != f)
     {
 
-        if(true==f.seek((f.size() - length),SeekMode::SeekSet))
+        if(false != f.seek(pos, SeekMode::SeekSet))
         {
-            if(true==FILE_read_internal(&f, context, length))
+            if(false != FILE_write_internal(&f, context,length))
             {
-                if(true==f.seek(pos*length, SeekMode::SeekSet))
-                {
-                    if(true==FILE_write_internal(&f, context,length))
-                    {
-                        ret=true;
-                    }
-                }
+                ret=true;
             }
         }
-
-    //bool a=FILE_truncate(name, (f.size()-length));
 
     f.close();  
     }
@@ -221,32 +233,11 @@ bool FILE_update(char *name, void *context, u32 length, u32 pos)
 }
 
 
-
-bool FILE_IsExists(char *name)
-{
-    return SPIFFS.exists(name);
-}
-
-size_t FILE_check_size(char *name)
-{
-    File f = SPIFFS.open(name, "r");
-    return f.size();
-}
-
-bool FILE_delete(char *name)
-{
-    bool ret = false;
-
-    if(true == SPIFFS.remove(name))
-    {
-        ret = true;
-    }
-    
-    return ret;
-}
-
-
-void FILE_get_List()
+//! Gives all filenames in "/" directory 
+/*!
+    \return : string, filenames seperated with comma in a string
+*/
+String FILE_get_file_list()
 {
     String path = "/";
     Dir dir =  SPIFFS.openDir(path);
@@ -258,23 +249,54 @@ void FILE_get_List()
         {
             output += ",";
         }
-        output += String(entry.name()).substring(1);     //entry.name() --> /test veriyor. / işaretini atmak için substring kullanıyoruz.
+        output += String(entry.fullName());    //name()).substring(1); //name ile fullname aynı sonucu veriyor
         entry.close();
     }
+
     output += "]";
-    Serial.println(output);
+    return output;
 }
 
-size_t FILE_dir_size(char* dirPath)
+size_t FILE_get_dir_size(char* dirPath)
 {
     size_t size=0;
 
     Dir dir = SPIFFS.openDir(dirPath);
-    
     while (dir.next())
     {
       size += dir.fileSize();  
     }
 
     return size;
+}
+
+size_t FILE_system_available_size()
+{
+    FSInfo FSInfo;
+    size_t available = 0;
+
+    if(false != SPIFFS.info(FSInfo))
+    {
+        available = FSInfo.totalBytes - FSInfo.usedBytes;
+    }
+
+    return available;
+}
+
+bool FILE_delete(char *name)
+{
+    return SPIFFS.remove(name);
+}
+
+bool FILE_IsExists(char *name)
+{
+    return SPIFFS.exists(name);
+}
+
+size_t FILE_get_size(char *name)
+{
+    File f = SPIFFS.open(name, "r");
+    size_t ret = f.size();
+    f.close();
+    return ret;
 }
